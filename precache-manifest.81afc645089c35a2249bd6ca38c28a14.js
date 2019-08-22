0f467529f49630a95fc1ecca0f2f5cc8326997a1
CarlosZiegler <carlos.ziegler@gmail.com>
2019-08-22 14:52:44 +0200

    Updates

---
+++ precache-manifest.81afc645089c35a2249bd6ca38c28a14.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbc80fb33cf6f559dc5e15769c71faa",
+    "url": "/profil-github-react-bootstrap/index.html"
+  },
+  {
+    "revision": "33099f94428b1f5b1891",
+    "url": "/profil-github-react-bootstrap/static/js/2.17e68d90.chunk.js"
+  },
+  {
+    "revision": "16015a9752028e6c4bdb",
+    "url": "/profil-github-react-bootstrap/static/js/main.73dcd82a.chunk.js"
+  },
+  {
+    "revision": "00d3b00d7fa6c8dc0743",
+    "url": "/profil-github-react-bootstrap/static/js/runtime~main.773f428e.js"
+  }
+]);
